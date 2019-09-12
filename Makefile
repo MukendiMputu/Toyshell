95e5012f331da2797ea43431c67fcef273318b4a
MukendiMputu <mukendi.mputu@tu-dortmund.de>
2019-09-12 00:40:49 -0600

    printing shell name, incrementing counter

--- Makefile
+++ Makefile
@@ -22,4 +22,4 @@ check : all
 
 # Clean up all build targets so that one may get a clean build
 clean :
-		rm -f *.o toyshell core*
+		rm -f *.o toyshell core* *core*
--- tsh.c
+++ tsh.c
@@ -3,30 +3,79 @@
 #include <string.h>
 #include <unistd.h>
 
-
-#define SH_NAME toyshell
-
-int main (int argc, char *argv []){
-    
-    
-    /* get shell name */
-
-    /* get the terminator */
-
-    /* initialize the counter */
-    int counter = 1;
-    
-    /* Loop  */
-
-    return 0;
-
-}
-
+void StopTheShell();
+void TokenizeCommandLine (char *tokens[], char *commandLine);
+int ExecuteShellProgram(char *prompt, int counter, char *terminator);
 
 #define MAX_TOKENS 1024
 
-void TokenizeCommandLine (char *tokens[], char *commandLine)
-{
+int main (int argc, char *argv []){
+    
+    // initialize counter, prompt and terminated 
+    int counter = 1;
+    char *prompt = (char *) malloc(sizeof(char)*256);
+    char *terminator =  (char *) malloc(sizeof(char)*256); 
+
+    // get and/or set the terminator 
+    prompt = "toyshell";
+    terminator = ">";
+    
+    // Loop 
+    ExecuteShellProgram(prompt, counter, terminator);
+    
+    // Free allocated memory above 
+    free(prompt);
+    free(terminator);
+
+    return 0;
+}
+
+/* Loop until the program is terminated */
+int ExecuteShellProgram(char *prompt, int counter, char *terminator) {
+    
+    // ToDo: display shell name, counter and prompt 
+    //       fetch the commandLine
+    //       tokenize the CL
+    //       execute the command
+   
+    char *rawCommand = (char *) malloc(sizeof(char)*256);
+    char *commandTokens = (char *) malloc(sizeof(char)*MAX_TOKENS);
+    size_t lineBuff = 0;
+    
+    // loop until the stop command is entered
+    while (1)
+    {
+        //print the prompt and wait for user to enter a command
+        printf("%s[%i]%s ", prompt, counter, terminator);
+        if(getline(&rawCommand, &lineBuff, stdin) != -1){
+            
+            /* TokenizeCommandLine(&commandTokens, rawCommand);
+            if(&commandTokens[0] == "stop")
+                StopTheShell(); */
+
+            if(*commandTokens != "\n")
+                counter++;
+
+        } else {
+            //perror("scanf");
+        }
+
+    }
+    
+    free(rawCommand);
+    free(commandTokens);
+
+    return 0;
+}
+
+void StopTheShell (){
+    return;
+}
+
+
+// Helper function to disect the command line
+void TokenizeCommandLine (char *tokens[], char *commandLine){
+    
     char *token [MAX_TOKENS];
     char *workCommandLine = (char*) malloc(sizeof(commandLine) + 1);
     int i;
@@ -35,7 +84,7 @@ void TokenizeCommandLine (char *tokens[], char *commandLine)
     for (i = 0; i < MAX_TOKENS; i++)
         tokens[i] = "";
     
-    strcpy (workCommandLine, &commandLine);
+    strcpy (workCommandLine, commandLine);
     i = 0;
     if ((token[i] = strtok(workCommandLine, " ")) != NULL){
         i++;
